3a3e4223c2a0745d3e57f89918603f40d1838ece
i-ajeesh <133880598+i-ajeesh@users.noreply.github.com>
2023-06-19 15:41:59 +0530

    Add files via upload
    Selenium Day 1 programs

---
+++ day1/Iphone.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class Iphone {
+
+	public void makeCall() {
+		System.out.println("'Siri: Call Gates'");
+	}
+
+	public void model() {
+		System.out.println("Mobile model is: 'iPhone 12 mini'");
+	}
+}
---
+++ day1/Smartphone.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Smartphone {
+
+	public static void main(String[] args) {
+		
+		Iphone maxpro = new Iphone();
+		maxpro.makeCall();
+		maxpro.model();
+
+	}
+
+}
